954b56620069d3aff15312eba40837ffb31e3e9b
ayyuby26 <nokian5220@gmail.com>
2023-06-08 08:54:44 +0700

    update sample UI stock item list

--- lib/features/dashboard/features/stock/presentation/pages/stock_page.dart
+++ lib/features/dashboard/features/stock/presentation/pages/stock_page.dart
@@ -18,6 +18,7 @@ class StockPageState extends State<StockPage> {
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
+        tooltip: "Add Stock",
         onPressed: () {},
         child: const Icon(Icons.add),
       ),
@@ -30,28 +31,118 @@ class StockPageState extends State<StockPage> {
               child: TextFormField(
                 controller: searchController,
                 decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: IColor.tertiary,
+                  ),
                   contentPadding: Const.edgesAll16,
-                  labelText: context.tr.finish,
+                  hintText: context.tr.finish,
                   focusedBorder: OutlineInputBorder(
                     borderRadius: Const.radiusCircular16,
-                    borderSide:
-                        BorderSide(color: IColor.tertiary.withOpacity(.3)),
+                    borderSide: BorderSide(
+                      color: IColor.tertiary.withOpacity(.3),
+                    ),
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: Const.radiusCircular16,
-                    borderSide:
-                        BorderSide(color: IColor.tertiary.withOpacity(.3)),
+                    borderSide: BorderSide(
+                      color: IColor.tertiary.withOpacity(.3),
+                    ),
                   ),
                   filled: true,
                   fillColor: Colors.white,
                 ),
               ),
             ),
-            const Center(
-              child: Text("Stock Page"),
+            const SizedBox(
+              height: 8,
+            ),
+            const Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    _Item(),
+                    SizedBox(height: 72),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _Item extends StatelessWidget {
+  const _Item();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.maxFinite,
+      padding: Const.edgesAll16,
+      margin: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: Const.radiusCircular8,
+        border: Border.all(
+          color: IColor.tertiary.withOpacity(.3),
+        ),
+      ),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  "Jahe Kencur",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Text(
+                "3 pcs",
+                // style: TextStyle(
+                //   fontWeight: FontWeight.bold,
+                //   fontSize: 18,
+                // ),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Rp 3.000"),
+              Text(
+                "EXP: 28 Jan 2020",
+                style: TextStyle(
+                  color: Colors.green,
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
